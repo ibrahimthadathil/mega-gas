3d39fed7eb4140a45d208c5341d42c11665dabe8
ibrahim <ibrahiminfo8@gmail.com>
2025-10-27 12:11:20 +0530

    add redux toolkit and auth slice for user authentication management

---
+++ src/redux/slice/auth/authSlicer.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface authState {
+  user_name: string | null;
+  is_authenticated: boolean;
+  role: string | null;
+  email: string | null;
+}
+
+const initialState: authState = {
+  user_name: null,
+  is_authenticated: false,
+  email: null,
+  role: null,
+};
+
+const userSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (
+      state,
+      action: PayloadAction<{ user_name: string; role: string; email: string }>
+    ) => {
+      state.user_name = action.payload.user_name;
+      state.email = action.payload.email;
+      state.role = action.payload.role;
+      state.is_authenticated = true;
+    },
+    logout: (state) => {
+      state.user_name = null;
+      state.email = null;
+      state.role = null;
+      state.is_authenticated = false;
+    },
+  },
+});
