e5ce184d92e3e8c34dae50f742a09935ae9983f3
ibrahim <ibrahiminfo8@gmail.com>
2025-11-25 19:24:12 +0530

    feat : changed all params into promise

--- src/app/api/user/warehouse/[id]/route.ts
+++ src/app/api/user/warehouse/[id]/route.ts
@@ -1,16 +1,18 @@
 import { STATUS } from "@/types/types";
 import { NextRequest, NextResponse } from "next/server";
 
-export const PUT = async (req: NextRequest,{params}:{params:{id:string}})=>{
+export const PUT = async (
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) => {
   try {
-    const {id} = await params;
-    const data = await req.json()
-    console.log('@@@',id,data);
-    return NextResponse.json({},{status:200})
+    const { id } = await context.params;
+    const data = await req.json();
+    return NextResponse.json({}, { status: 200 });
   } catch (error) {
-      return NextResponse.json(
-          { error: (error as Error).message },
-          { status: STATUS.SERVER_ERROR.code }
-        );
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: STATUS.SERVER_ERROR.code }
+    );
   }
-}
+};
