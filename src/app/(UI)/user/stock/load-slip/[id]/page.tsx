7c4f06faa20828c5114ace2df8e2a96f8882a390
ibrahim <ibrahiminfo8@gmail.com>
2026-01-27 10:59:09 +0530

    -fix : just added a ker to the trip load

--- src/app/(UI)/user/stock/load-slip/[id]/page.tsx
+++ src/app/(UI)/user/stock/load-slip/[id]/page.tsx
@@ -3,7 +3,7 @@ import TripSheet from "@/app/(UI)/user/stock/_UI/trip-sheet"
 export default function Home({ params }: { params: { id: string } }) {
   return (
     <main className="min-h-screen bg-background p-4 sm:p-6">
-      <TripSheet loadSlipId={params.id}/>
+      <TripSheet key={params.id} loadSlipId={params.id}/>
     </main>
   )
 }
